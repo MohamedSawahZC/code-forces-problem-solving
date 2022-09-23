4e14d022523b7dc409797e02cfc0957990bb640f
Mohamed Sawah <sawah810@gmail.com>
2022-09-23 04:01:32 +0200

    update

--- bear_and_big_brother.cpp
+++ bear_and_big_brother.cpp
@@ -1,27 +1,16 @@
 #include <bits/stdc++.h>
-#include <string>
+
 
 using namespace std;
 
-
-
-int main(){
-    int a,b,y=0;
+int main()
+{
+    int a,b,years=0;
     cin>>a>>b;
-    while(true){
-
-        if(a<b){
-                     a*=3;
-       b*=2;
-            y++;
-
-        }else if(a==b){
-            y++;
-            cout<<y;
-            return false;
-        }else{
-         cout<<y;
-            return false;
-        }
+    while(a<=b){
+        a*=3;
+        b*=2;
+        years++;
     }
+    cout<<years;
 }
